a6947b760e40f94112486a7d969e51da6a51cf09
Levuloopse <kirigirisayaka@gmail.com>
2018-04-14 15:00:41 +0800

    0414

---
+++ temp/CardGame.js
@@ -0,0 +1,16 @@
+let NiuNiu=0;
+let YangYang=0;
+let n = prompt("请输入纸牌数");
+let m= prompt("请输入纸牌上的数字");
+
+if(isNaN(n)||isNaN(m)){
+  for(let count=0;count<n;count++){
+    if(count/2){
+      NiuNiu += Max(m);
+      delect(Max(m));
+    }
+    else Yangyang +=Max(m)
+    delect(Max(m));
+  }
+}
+
